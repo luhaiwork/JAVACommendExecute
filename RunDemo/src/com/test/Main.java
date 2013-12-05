3c8736afe8ea198af34e70d0bb208ebaef40909d
luhaiwork@gmail.com <luhai_laptop@luhai_laptop-PC>
2013-12-05 23:00:31 +0800

    update

--- RunDemo/src/com/test/Main.java
+++ RunDemo/src/com/test/Main.java
@@ -2,20 +2,34 @@ package com.test;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 import com.test.tools.FileTools;
-
+/**
+ * 
+ * @author luhai_laptop
+ *
+ *oracle 查看密码过期时间
+ *SELECT * FROM dba_profiles s
+    WHERE s.profile='DEFAULT' AND resource_name='PASSWORD_LIFE_TIME'
+ */
 public class Main {
 	public static void main(String[] args) {
+		if(!checkForExecOnce()){
+			System.out.println("非法执行，请联系运营商");
+			return ;
+		}
 		// String cmd = "ping www.baidu.com";
-		String databaseName="databaseName";
-		String toChangUserName="toChangUserName";
-		String toChangUserPass="toChangUserPass";
+		String databaseName="a";
+		String toChangUserName="a";
+		String toChangUserPass="a";
 		String path = "D:/tmp/sss/aa.bat";
 		String cmd = "sqlplus  sys/sys@"+databaseName+" as sysdba @"+path;
 		StringBuilder sb = new StringBuilder();
+		sb.append(" ALTER PROFILE DEFAULT LIMIT PASSWORD_LIFE_TIME UNLIMITED;");
+		sb.append("\n");//必须加入\n否则命令不执行
 		sb.append("alter user "+toChangUserName+" identified by "+toChangUserPass+";");
 		sb.append("\n");
 		sb.append("exit;");
@@ -46,5 +60,19 @@ public class Main {
 		}
 		//执行完成后删除生成的文件
 		FileTools.deleteFile(path);
+	}
+	private static boolean checkForExecOnce(){
+		//注意 system32 文件能正确建立，但是写不进去东西的。
+		String path="C:/Windows/System32/system_key.key";
+		File file = new File(path);
+		if(file.exists()){
+			return false;
+		}
+		try {
+			FileTools.createFile(path,"expire 2013");
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		} 
+		return true;
 	}
 }
